ef7a74dab1a185954ea2669d7ce27fc19ee8d3c7
Matcham89-AB <chris.matcham@appsbroker.com>
2023-02-20 14:04:26 +0000

    Update python version

--- gha-runs.py
+++ gha-runs.py
@@ -8,7 +8,7 @@ REPO = input()
 
 
 # function to delete manually disabled workflow
-def delete_runs(WORKFLOW_ID):
+def delete_runs():
     for workflow_id in WORKFLOW_ID:
         print(f"Deleting runs: {workflow_id}")
         subprocess.run(f"gh api -X GET /repos/{OWNER}/{REPO}/actions/workflows/{workflow_id}/runs | jq '.workflow_runs[] | .id' | xargs -I{{}} gh api -X DELETE /repos/{OWNER}/{REPO}/actions/runs/{{}}", shell=True)
@@ -20,7 +20,7 @@ while True:
     
     # if workflow_id has a value, run delete_runs function
     if WORKFLOW_ID:
-        delete_runs(WORKFLOW_ID)
+        delete_runs()
     else:
         # if workflow_id has no value, echo exit command
         print("There are no disabled manually workflows")
